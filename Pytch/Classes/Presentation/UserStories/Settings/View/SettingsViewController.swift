48dab1d484158f749fa23e98f6a7d3c19157446e
Rinat Enikeev <rinat.enikeev@gmail.com>
2018-10-05 15:32:45 +0500

    implement settings view input in settings view controller

---
+++ Pytch/Classes/Presentation/UserStories/Settings/View/SettingsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsViewController.swift
+//  Pytch
+//
+//  Created by Rinat Enikeev on 05/10/2018.
+//  Copyright © 2018 Aparlay Limited. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    var output: SettingsViewOutput!
+}
+
+extension SettingsViewController: SettingsViewInput {
+    
+}
