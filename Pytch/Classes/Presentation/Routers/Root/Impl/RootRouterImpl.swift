2d294593116cfa225dd5d0ad3341216b0af9a425
Rinat Enikeev <rinat.enikeev@gmail.com>
2018-10-09 12:06:53 +0500

    assert previous view controller is nil

--- Pytch/Classes/Presentation/Routers/Root/Impl/RootRouterImpl.swift
+++ Pytch/Classes/Presentation/Routers/Root/Impl/RootRouterImpl.swift
@@ -12,12 +12,17 @@ class RootRouterImpl: RootRouter {
     
     static let shared = RootRouterImpl()
     
+    weak var previousRootViewController: UIViewController?
+    
     func openLogin() {
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        previousRootViewController = appDelegate.window?.rootViewController
         let storyboard = UIStoryboard(name: "Login", bundle: .main)
         let rootController = storyboard.instantiateInitialViewController()
         appDelegate.window = UIWindow(frame: UIScreen.main.bounds)
         appDelegate.window?.rootViewController = rootController
         appDelegate.window?.makeKeyAndVisible()
+        
+        assert(previousRootViewController == nil)
     }
 }
