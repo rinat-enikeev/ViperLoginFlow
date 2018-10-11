85e0b8615ba6f6d69affbffd0feca75419aedc42
Rinat Enikeev <rinat.enikeev@gmail.com>
2018-10-11 11:52:03 +0500

    make code same as Rico's

--- Pytch/Classes/Presentation/Routers/Root/Impl/RootRouterImpl.swift
+++ Pytch/Classes/Presentation/Routers/Root/Impl/RootRouterImpl.swift
@@ -12,16 +12,11 @@ class RootRouterImpl: RootRouter {
     
     static let shared = RootRouterImpl()
     
-    weak var previousRootViewController: UIViewController?
-    
     func openLogin() {
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        previousRootViewController = appDelegate.window?.rootViewController
         let storyboard = UIStoryboard(name: "Login", bundle: .main)
         let rootController = storyboard.instantiateInitialViewController()
         appDelegate.window?.rootViewController = rootController
         appDelegate.window?.makeKeyAndVisible()
-        
-        assert(previousRootViewController == nil)
     }
 }
